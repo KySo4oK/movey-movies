250faccaccb0b12522cb38ab33f9ca8de381495d
KySo4oK <prostorostuk08@gmail.com>
2021-03-19 19:38:17 +0200

    add eureka server

--- src/main/java/micro/movie/service/movieservice/MovieServiceApplication.java
+++ src/main/java/micro/movie/service/movieservice/MovieServiceApplication.java
@@ -2,12 +2,12 @@ package micro.movie.service.movieservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
+@EnableEurekaServer
 public class MovieServiceApplication {
 
     public static void main(String[] args) {
