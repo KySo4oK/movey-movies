e06ffa69418b2094b5a0b381bee30f645d65b7cc
KySo4oK <prostorostuk08@gmail.com>
2021-03-26 00:41:27 +0200

    change services to use rest template

--- src/main/java/micro/movie/service/movieservice/HelloController.java
+++ src/main/java/micro/movie/service/movieservice/HelloController.java
@@ -1,14 +1,19 @@
 package micro.movie.service.movieservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movie")
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello! Movie microservice is on Kubernetes now!";
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "http://localhost:8081/user/hello";
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+        return response.getBody() + "Hello! Movie microservice is on Kubernetes now!";
     }
 }
