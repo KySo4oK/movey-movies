3f1c133954f28dac4daa69ba7016310e98bf10db
KySo4oK <prostorostuk08@gmail.com>
2021-03-26 21:02:46 +0200

    implement hystrix circuit breaker

--- src/main/java/micro/movie/service/movieservice/HelloController.java
+++ src/main/java/micro/movie/service/movieservice/HelloController.java
@@ -1,5 +1,7 @@
 package micro.movie.service.movieservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ public class HelloController {
     UserService service;
 
     @GetMapping("/hello")
+
     public String sayHello() {
         String n;
         while (true) {
@@ -28,5 +31,29 @@ public class HelloController {
             }
             return n + "Hello! Movie microservice is on Kubernetes now!";
         }
+    }
+
+    @GetMapping("/hi")
+    @HystrixCommand(fallbackMethod = "defaultMethod",
+            threadPoolKey = "getProductThreadPool",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "1000"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "20000"),
+                    @HystrixProperty(name = "metrics.healthSnapshot.intervalInMilliseconds", value = "5000"),
+                    @HystrixProperty(name = "fallback.isolation.semaphore.maxConcurrentRequests", value = "5")
+            },
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "30"),
+                    @HystrixProperty(name = "maxQueueSize", value = "-1"),
+            })
+    public String sayHi() {
+        return service.hello() + "HI! Movie microservice is on Kubernetes now!";
+    }
+
+
+    private String defaultMethod() {
+        return "User unavailable";
     }
 }
--- src/main/java/micro/movie/service/movieservice/UserService.java
+++ src/main/java/micro/movie/service/movieservice/UserService.java
@@ -3,7 +3,7 @@ package micro.movie.service.movieservice;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "user-service", url = "user-service-service.default.svc.cluster.local")
+@FeignClient(name = "user-service", url = "http://localhost:8080")
 public interface UserService {
     @GetMapping(value = "/user/hello")
     String hello();
